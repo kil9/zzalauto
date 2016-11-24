d8875b78ed447fca62f5985529e792a943374318
kil9 <krieiter@gmail.com>
2016-11-24 17:42:46 +0900

    Add local running script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+N_PIC=10
+PORT=5000
+
+case "$1" in
+  run)
+    N_PIC=${2:=N_PIC}
+    curl localhost:${PORT}/run/${N_PIC}
+  ;;
+
+  *)
+    tmux split-window -v './web.sh'
+    tmux split-window -v './worker.sh'
+    sleep 5
+    curl localhost:${PORT}/run/${N_PIC}
+  ;;
+esac
